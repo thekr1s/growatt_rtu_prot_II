f912e2e49121da934fc2199cfd941222bac68c0a
Robert Wassens <robert@wssns.nl>
2023-01-04 19:05:40 +0100

    initial working protocol-II

--- example.py
+++ example.py
@@ -4,6 +4,9 @@ from sys import argv
 
 from growattRS232 import GrowattRS232
 
+class TestRegs:
+    registers = []
+    
 # defaults
 # USB port of RS232 converter
 DEFAULT_PORT = "/dev/ttyUSB0"
@@ -12,18 +15,16 @@ DEFAULT_ADDRESS = 0x1
 
 logging.basicConfig(level=logging.DEBUG)
 
+port = str(argv[1]) if len(argv) > 1 else DEFAULT_PORT
+address = int(argv[2]) if len(argv) > 2 else DEFAULT_ADDRESS
+growattRS232 = GrowattRS232(port, address)
 
-async def main():
-    port = str(argv[1]) if len(argv) > 1 else DEFAULT_PORT
-    address = int(argv[2]) if len(argv) > 2 else DEFAULT_ADDRESS
-    growattRS232 = GrowattRS232(port, address)
-    try:
-        data = await growattRS232.async_update()
-        print(f"Sensors data: {data}")
-    except Exception as error:
-        print("Error: " + repr(error))
+rir1 = TestRegs()
+rir1.registers = [1, 0, 232, 1509, 0, 0, 159, 1151, 0, 0, 73, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 221, 4997, 2317, 2, 0, 278, 0, 0, 0, 0, 0, 0, 0, 0, 2317, 0, 0, 0, 8, 0, 6793, 136, 31879, 0, 4, 0, 3783, 0, 4, 0, 3347, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7130]
+rir1.registers = [1, 0, 68, 855, 0, 0, 66, 674, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 65, 4998, 2308, 1, 0, 149, 0, 0, 0, 0, 0, 0, 0, 0, 2308, 0, 0, 0, 8, 0, 6793, 136, 34393, 0, 4, 0, 3783, 0, 4, 0, 3347, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7130]
+data = growattRS232.input_regs_to_data(rir1)
+print(f"Sensors data: {data}")
+# exit()
+data = growattRS232.sync_update()
+print(f"Sensors data: {data}")
 
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-loop.close()
--- growattRS232/__init__.py
+++ growattRS232/__init__.py
@@ -123,8 +123,6 @@ class GrowattRS232:
         on the firmware version of your inverter.
         """
 
-        data = {}
-
         if not os.path.exists(self._port):
             self._logger.debug(f"USB port {self._port} is not available")
             raise PortException(f"USB port {self._port} is not available")
@@ -192,50 +190,49 @@ class GrowattRS232:
                 )
             )
 
-        rir1 = self._client.read_input_registers(0, 44, unit=self._unit)
+        rir1 = self._client.read_input_registers(0, 125, unit=self._unit)
         if rir1.isError():
             self._client.close()
             self._logger.debug("Modbus read failed for rir1.")
             raise ModbusException("Modbus read failed for rir1.")
 
-        rir2 = self._client.read_input_registers(45, 21, unit=self._unit)
-        if rir2.isError():
-            self._client.close()
-            self._logger.debug("Modbus read failed for rir2.")
-            raise ModbusException("Modbus read failed for rir2")
 
         self._client.close()
 
+        print(f"rir1 {rir1.registers}")
+        data = self.input_regs_to_data(rir1)
         # Inverter properties
         data[ATTR_SERIAL_NUMBER] = self._serial_number
         data[ATTR_MODEL_NUMBER] = self._model_number
         data[ATTR_FIRMWARE] = self._firmware
+        self._last_update = datetime.now()
+        return data
 
+    def input_regs_to_data(self, rir1):
+        data = {}
+        
         # DC input PV
         data[ATTR_INPUT_POWER] = rsdf(rir1, 1)
-        data[ATTR_INPUT_ENERGY_TOTAL] = rsdf(rir2, 11)
+        data[ATTR_INPUT_ENERGY_TOTAL] = rsdf(rir1, 55)
 
         # AC output grid
         data[ATTR_OUTPUT_POWER] = rsdf(rir1, 11)
         data[ATTR_OUTPUT_ENERGY_TODAY] = rsdf(rir1, 26)
         data[ATTR_OUTPUT_ENERGY_TOTAL] = rsdf(rir1, 28)
-        data[ATTR_OUTPUT_REACTIVE_POWER] = rsdf(rir2, 13)
-        data[ATTR_OUTPUT_REACTIVE_ENERGY_TODAY] = rsdf(rir2, 15)
-        data[ATTR_OUTPUT_REACTIVE_ENERGY_TOTAL] = rsdf(rir2, 17)
 
         # DC input string 1 PV
         data[ATTR_INPUT_1_VOLTAGE] = rssf(rir1, 3)
         data[ATTR_INPUT_1_AMPERAGE] = rssf(rir1, 4)
         data[ATTR_INPUT_1_POWER] = rsdf(rir1, 5)
-        data[ATTR_INPUT_1_ENERGY_TODAY] = rsdf(rir2, 3)
-        data[ATTR_INPUT_1_ENERGY_TOTAL] = rsdf(rir2, 5)
+        data[ATTR_INPUT_1_ENERGY_TODAY] = rsdf(rir1, 59)
+        data[ATTR_INPUT_1_ENERGY_TOTAL] = rsdf(rir1, 61)
 
         # DC input string 2 PV
         data[ATTR_INPUT_2_VOLTAGE] = rssf(rir1, 7)
         data[ATTR_INPUT_2_AMPERAGE] = rssf(rir1, 8)
         data[ATTR_INPUT_2_POWER] = rsdf(rir1, 9)
-        data[ATTR_INPUT_2_ENERGY_TODAY] = rsdf(rir2, 7)
-        data[ATTR_INPUT_2_ENERGY_TOTAL] = rsdf(rir2, 9)
+        data[ATTR_INPUT_2_ENERGY_TODAY] = rsdf(rir1, 63)
+        data[ATTR_INPUT_2_ENERGY_TOTAL] = rsdf(rir1, 65)
 
         # AC output phase 1 grid
         data[ATTR_OUTPUT_1_VOLTAGE] = rssf(rir1, 14)
@@ -259,22 +256,18 @@ class GrowattRS232:
         data[ATTR_IPM_TEMPERATURE] = rssf(rir1, 41)
         data[ATTR_P_BUS_VOLTAGE] = rssf(rir1, 42)
         data[ATTR_N_BUS_VOLTAGE] = rssf(rir1, 43)
-        data[ATTR_DERATING_MODE] = rir2.registers[2]
-        data[ATTR_DERATING] = DERATINGMODES[rir2.registers[2]]
+        # data[ATTR_DERATING_MODE] = rssf(rir1, 104)
+        # data[ATTR_DERATING] = DERATINGMODES[rssf(rir1, 104)]
 
         # Status, faults & warnings
         data[ATTR_STATUS_CODE] = rir1.registers[0]
         data[ATTR_STATUS] = STATUSCODES[rir1.registers[0]]
         data[ATTR_FAULT_CODE] = rir1.registers[40]
         data[ATTR_FAULT] = FAULTCODES[rir1.registers[40]]
-        data[ATTR_WARNING_CODE] = rir2.registers[19]
-        data[ATTR_WARNING] = WARNINGCODES[rir2.registers[19]]
-        data[ATTR_WARNING_VALUE] = rir2.registers[20]
+        # data[ATTR_WARNING_CODE] = rsdf(rir1, 110, 1)
+        # data[ATTR_WARNING] = WARNINGCODES[rir2.registers[19]]
+        # data[ATTR_WARNING_VALUE] =  rssf(rir1, 112, 1)
 
-        if not data:
-            return {}
-
-        self._last_update = datetime.now()
         return data
 
 
